29de5ff95415c7899ed034d24249964887f09748
James Schaffer <jas.schaffer@gmail.com>
2019-10-08 16:14:46 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,28 @@
-# Code your solution here!
+require "pry"
+
+def generate_goal
+  rand(6) + 1
+end
+
+
+def capture_input 
+  input = gets.chomp
+end
+
+def compare(goal, input)
+  case input
+    when goal
+      puts "You guessed the correct number!"
+    when "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry! The computer guessed #{goal}."
+  end
+end
+
+def run_guessing_game
+  goal = generate_goal.to_s
+  input = capture_input
+  compare(goal, input)
+end
+
